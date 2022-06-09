affdfc1d0ad7db19ea903058be5bd8d5a2afc34b
VIKAS BHAGATH <60500684+bhagathvikas@users.noreply.github.com>
2022-06-09 17:17:25 +0000

    setting up everythimg

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import {ChainId,ThirdwebProvider} from '@thirdweb-dev/react';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  <ThirdwebProvider
+    desiredChainId = {ChainId.Rinkeby}
+    ChainRpc = {{[ChainId.Rinkeby] : "https://rinkeby.infura.io/v3/fb18c917041c49ee936ddf1708c2fae9"}}
+    >
+ <Component {...pageProps} />
+    </ThirdwebProvider>
 }
 
 export default MyApp
